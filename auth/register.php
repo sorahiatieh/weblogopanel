a7d1f127df888798303c48cc6ef753abc56ae078
atieh <a.sorahitalab@gmail.com>
2025-12-06 18:27:37 +0330

    compleat register page

--- auth/register.php
+++ auth/register.php
@@ -3,6 +3,27 @@
 	
 	global $conn;
     $error="";
+    if(isset($_POST['submit'])){
+        if(
+                isset($_POST['name']) && !empty($_POST['name'])
+        && isset($_POST['email']) && !empty($_POST['email'])
+            && isset($_POST['password']) && !empty($_POST['password'])){
+        if($_POST['password']===$_POST['confirmPassword']){
+            $hashedPassword=password_hash($_POST['password'],PASSWORD_DEFAULT);
+            $sql="INSERT INTO users SET name=?, email=?, password=?";
+            $stmt=$conn->prepare($sql);
+            $stmt->execute([$_POST['name'],$_POST['email'],$hashedPassword]);
+            header("location:login.php");
+        }
+        else{
+            $error="مقدار پسورد و تکرار آن برابر نیستند!";
+        }
+    }
+    else{
+        $error="لطفا همه فیلدها را پر کنید!";
+    }
+    }
+	
     ?>
 <html dir="rtl" lang="fa-IR">
 <head>
@@ -20,19 +41,19 @@
                 <div class="col-lg-6">
                     <div class="card-body p-4 text-center">
                             <a href="#"><img src="../assets/Img/logo-main.png" alt="logo" class="pt-2 pb-4"></a>
-                        <h3><?php if($error !== ""){echo $error;} ?></h3>
+                        <p><?php if($error !== ""){echo $error;} ?></p>
                         <form action="register.php" method="post">
                             <div class="form-group">
-                                <input type="text" name="name" id="name" class="form-control" required placeholder="نام و نام خانوادگی">
+                                <input type="text" name="name" id="name" class="form-control"  placeholder="نام و نام خانوادگی">
                             </div>
                             <div class="form-group">
-                                <input type="email" name="mail" id="mail" class="form-control" placeholder="آدرس ایمیل">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="آدرس ایمیل">
                             </div>
                             <div class="form-group">
-                                <input type="password" name="password" id="password" class="form-control" required placeholder="کلمه عبور">
+                                <input type="password" name="password" id="password" class="form-control"  placeholder="کلمه عبور">
                             </div>
                             <div class="form-group mb-4">
-                                <input type="password" name="confirmPassword" id="confirmPassword" class="form-control" required placeholder="تکرار کلمه عبور">
+                                <input type="password" name="confirmPassword" id="confirmPassword" class="form-control"  placeholder="تکرار کلمه عبور">
                             </div>
                             <button type="submit" name="submit" id="register" class="btn btn-block btn-success py-2 radius10 mb-4">عضویت</button>
                          </form>
