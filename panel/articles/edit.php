e10eec96bb3bcf5ea6300b510bcaa89206519232
atieh <a.sorahitalab@gmail.com>
2026-01-07 12:56:53 +0330

    edit articles

--- panel/articles/edit.php
+++ panel/articles/edit.php
@@ -1,80 +1,110 @@
-<?php require_once "../../functions/helpers.php" ?>
-<?php require_once "../../functions/users.php" ?>
 <?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header('location: ../auth/login.php');
-}
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $user = getUser($id);
-}
-
-$error = "";
-$message = "";
-if (isset($_POST['submit'])) {
-    if (isset($_POST['name']) && !empty($_POST['name'])) {
-        if(isset($_POST['password']) && !empty($_POST['password'])){
-            if (checkPassword($_POST['password'])) {
-                updateUser($id, $_POST['name'], $_POST['password'],$_FILES['image']);
-                //$message = "کاربر ویرایش شد";
-                header('location: index.php');
-            } else {
-                $error = "رمز عبور شما حداقل باید 6 کاراکتر باشد";
-            }
-        }else{
-            updateUser($id, $_POST['name'], $_POST['password'],$_FILES['image']);
-            //$message = "کاربر ویرایش شد";
-            header('location: index.php');
-        }
-    } else {
-        $error = "لطفا تمام فیلد ها را تکمیل کنید";
-    }
-}
-
+	require_once "../../functions/helpers.php";
+	require_once "../../functions/articles.php";
+	require_once "../../functions/categories.php";
+	require_once "../../functions/users.php";
+	
+	session_start();
+	
+	if(!isset($_SESSION['user'])){
+		header('location: ../auth/login.php');
+	}
+	$user_id=checkUser($_SESSION['user'])->id;
+	
+	$categories=getAllCategories();
+	
+	if(isset($_GET['id'])){
+		$id=$_GET['id'];
+	}
+	
+	$article=getArticles($id);
+	$error="";
+	$message="";
+	if(isset($_POST['submit'])){
+		if(
+			isset($_POST['title']) && !empty($_POST['title']) && isset($_POST['body']) && !empty($_POST['body']) && isset($_POST['category_id']) && !empty($_POST['category_id']) && isset($_POST['status']) && !empty($_POST['status']) && isset($_FILES['image']) && !empty($_FILES['image']['name'])
+		){
+			updateArticle($id,$_POST['title'],$_POST['body'],$_POST['category_id'],$_POST['status'],$_FILES['image']);
+			header('location: index.php');
+		}
+		else{
+			$error="لطفا تمام فیلد ها را تکمیل کنید";
+		}
+	}
+	include "../layouts/head.php";
+	include "../layouts/navigation.php";
+	include "../layouts/header.php";
 
 ?>
-<?php include "../layouts/head.php" ?>
-<?php include "../layouts/navigation.php" ?>
-<?php include "../layouts/header.php" ?>
     <main class="main-content">
         <div class="card">
             <div class="card-body">
-                <p style="color: red;"><?php if ($error !== "") {
-                        echo $error;
-                    } ?>  </p>
-                <p style="color: green;"><?php if ($message !== "") {
-                        echo $message;
-                    } ?>  </p>
+                <p style="color: red;"><?php if($error!==""){
+						echo $error;
+					} ?>  </p>
+                <p style="color: green;"><?php if($message!==""){
+						echo $message;
+					} ?>  </p>
                 <div class="container">
-                    <h6 class="card-title">ویرایش کاربر</h6>
-                    <form method="POST" action="edit.php?id=<?php echo $user->id ?>" enctype="multipart/form-data">
+                    <h6 class="card-title">ویرایش مقاله</h6>
+                    <form method="POST" action="edit.php?id=<?php echo $article->id ?>" enctype="multipart/form-data">
                         <div class="form-group row">
-                            <figure class="avatar avatar">
-                                <img src="<?php echo asset('images/') . $user->image ?>" class="rounded-circle"
-                                     alt="image">
-                            </figure>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">نام و نام خانوادگی</label>
+                            <label class="col-sm-2 col-form-label">عنوان مقاله</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control text-left" dir="rtl" name="name"
-                                       value="<?php echo $user->name ?>">
+                                <input type="text" class="form-control text-left" dir="rtl" name="title"
+                                       value="<?php echo $article->title ?>">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">ایمیل</label>
+                            <label class="col-sm-2 col-form-label">متن مقاله</label>
                             <div class="col-sm-10">
-                                <input disabled type="text" class="form-control text-left" dir="rtl" name="email"
-                                       value="<?php echo $user->email ?>">
+                                <textarea class="form-control text-left" dir="rtl" name="body" cols="30" rows="10">
+                                    <?php echo $article->body ?>
+                                </textarea>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">پسورد</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control text-left" dir="rtl" name="password">
-                            </div>
+                            <lable for="exampleFromControlSelect1">دسته بندی</lable>
+                            <select class="form-control" name="category_id" id="exampleFromControlSelect1">
+								<?php
+									foreach($categories as $category){
+										?>
+										<?php if($category->id==$article->category_id){ ?>
+                                            <option selected
+                                                    value="<?= $category->id ?>"><?= $category->title ?></option>
+											<?php
+										}
+										else{ ?>
+                                            ?>
+                                            <option value="<?= $category->id ?>"><?= $category->title ?></option>
+											<?php
+										}
+									}
+								?>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <lable for="exampleFromControlSelect2">وضعیت</lable>
+                            <select class="form-control" name="status" id="exampleFromControlSelect2">
+								<?php
+									if($article->status==="draft"){?>
+                                        <option selected value="draft">پیش نویس</option>
+										<?php }else{ ?>
+                                         <option value="draft">پیش نویس</option>
+                                        <?php } ?>
+								<?php
+									if($article->status==="published"){?>
+                                        <option selected value="published">منتشر شده</option>
+									<?php }else{ ?>
+                                        <option value="published">منتشر شد</option>
+									<?php } ?>
+								<?php
+									if($article->status==="archived"){?>
+                                        <option selected value="archived">بایگانی</option>
+									<?php }else{ ?>
+                                        <option value="archived">بایگانی</option>
+									<?php } ?>
+                            </select>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label" for="file"> آپلود عکس </label>
